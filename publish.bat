939708d262c7b613f088af1114383cdabb106c64
geqi <jiangsugeqi@outlook.com>
2019-06-08 13:25:54 +0800

    add new blog

---
+++ publish.bat
@@ -0,0 +1,9 @@
+call hexo g
+call cd public
+call git add .
+call git commit -m "add new blog"
+call git push origin master
+call cd ..
+call git add .
+call git commit -m "add new blog"
+call git push origin master
